2be9541666a9592492fcafaabbc8c537fdc4e5d1
Adamina Flores <adaminaflores19@gmail.com>
2021-12-09 13:12:19 -0600

    Finished inputting and reconstructing the servlets, dao, and models. Accidentally didn't create own branch so pushing straight up to main

---
+++ src/main/java/dao/Posts.java
@@ -0,0 +1,14 @@
+package dao;
+
+//import com.codeup.Postlister.models.Post;
+
+import models.Post;
+
+import java.util.List;
+
+public interface Posts {
+
+    List<Post> all();
+
+    Long insert(Post post);
+}
