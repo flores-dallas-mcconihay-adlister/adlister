b15b12050913bd8e0f3cc1b36c0d7e2938f8e30d
Adamina Flores <adaminaflores19@gmail.com>
2021-12-13 17:22:09 -0600

    trying to do single post servlet

---
+++ src/main/java/controllers/IndividualPostServlet.java
@@ -0,0 +1,8 @@
+package controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(name = "")
+public class IndividualPost extends HttpServlet {
+
+}
