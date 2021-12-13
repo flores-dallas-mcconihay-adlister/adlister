9394565ddda25472bcd958a2be6dd18ba2f63890
Ian McConihay <mcconihay.ian92@gmail.com>
2021-12-13 14:37:21 -0600

    fixed exception

--- src/main/java/controllers/SearchServlet.java
+++ src/main/java/controllers/SearchServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet(name = "controllers.ServerServlet", urlPatterns = "/index")
@@ -17,12 +16,8 @@ public class SearchServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String searchString = request.getParameter("s");
-		try {
-			List<Post> postList = DaoFactory.getPostsDao().searchPost(searchString);
-			request.setAttribute("posts", postList);
-		} catch (SQLException se) {
-			se.printStackTrace();
-		}
+		List<Post> postList = DaoFactory.getPostsDao().searchPost(searchString);
+		request.setAttribute("posts", postList);
 
 		request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
 	}
