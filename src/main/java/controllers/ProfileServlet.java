25b3c48e068d8c638f8c7be56b496e443d1c642e
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-13 23:10:54 -0600

    edited profile servlet

--- src/main/java/controllers/ProfileServlet.java
+++ src/main/java/controllers/ProfileServlet.java
@@ -9,19 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "controllers.ProfileServlet", urlPatterns = "/profile")
 public class ProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        User registeredUser = (User) request.getSession().getAttribute("user");
-        User userPost = DaoFactory.getUsersDao().findByUsername(registeredUser.getUsername());
-        request.setAttribute("posts", DaoFactory.getPostsDao().allById(userPost.getId()));
+        User user = DaoFactory.getUsersDao().findByUsername((String) request.getSession().getAttribute("user"));
+
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+        } else {
+            request.setAttribute("posts", DaoFactory.getPostsDao().allById(user.getId()));
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        }
+
+//        User registeredUser = (User) request.getSession().getAttribute("user");
+//        User userPost = DaoFactory.getUsersDao().findByUsername(registeredUser.getUsername());
+//        request.setAttribute("posts", DaoFactory.getPostsDao().allById(userPost.getId()));
 
 
 
-        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+//        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 //    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 //        String username = request.getParameter("username");
