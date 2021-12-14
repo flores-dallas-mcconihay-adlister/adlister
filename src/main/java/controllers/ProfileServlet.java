661e028cd08aa3d2eb997d2e6c9077fa2715da08
Ian McConihay <mcconihay.ian92@gmail.com>
2021-12-14 10:06:09 -0600

    profile

--- src/main/java/controllers/ProfileServlet.java
+++ src/main/java/controllers/ProfileServlet.java
@@ -15,40 +15,10 @@ import java.sql.SQLException;
 public class ProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        User user = DaoFactory.getUsersDao().findByUsername((String) request.getSession().getAttribute("user"));
         User registeredUser = (User) request.getSession().getAttribute("user");
         User userPost = DaoFactory.getUsersDao().findByUsername(registeredUser.getUsername());
         request.setAttribute("post", DaoFactory.getPostsDao().allById(userPost.getId()));
-
-
-//         System.out.println(DaoFactory.getPostsDao().allById(userPost.getId()));
-//         if (request.getSession().getAttribute("user") == null) {
-//             response.sendRedirect("/login");
-//         } else {
-//             request.setAttribute("posts", DaoFactory.getPostsDao().allById(user.getId()));
-//             request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-//         }
-
-//        User registeredUser = (User) request.getSession().getAttribute("user");
-//        User userPost = DaoFactory.getUsersDao().findByUsername(registeredUser.getUsername());
-//        request.setAttribute("posts", DaoFactory.getPostsDao().allById(userPost.getId()));
-
-
-
-
-//        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String username = request.getParameter("username");
-//        String password = request.getParameter("password");
-//        User user = DaoFactory.getUsersDao().findByUsername(username);
-//
-//        if (user != null) {
-//            request.setAttribute("posts", DaoFactory.getPostsDao().all());
-//            return;
-//        }
-//
-//    }
 }
 
